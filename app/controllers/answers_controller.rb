1a2ac837ac4bf250fd2505ff3cfe188029faf6c6
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-07 17:31:31 -0400

    Untested anchor on question page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,9 @@
 class AnswersController < ApplicationController
+
+  def show
+    answer = Answer.find(params[:id])
+    @question = answer.question
+
+    redirect_to(question_url(@question.id, anchor: answer.id))
+  end
 end

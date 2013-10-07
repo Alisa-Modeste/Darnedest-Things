ad3f21520374c622d546179e176487461786bcc0
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-07 15:45:47 -0400

    Set current_user; Update user and session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < ApplicationController
 
+  def new
+    render :new
+  end
+
   def create
+    user = User.find_by_credentials(username, password)
+
+    if user
+      self.current_user = user
+      redirect_to root_url
+    else
+      flash.now[:notice] = "Invalid credentials"
+      render :new
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(params[:user])
+
+    if user.save
+      self.current_user = user
+      redirect_to root_url
+    else
+      flash.now[:notice] = user.errors.full_messages
+      render :new
+    end
 end

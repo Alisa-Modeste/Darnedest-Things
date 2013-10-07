a5de34fee169ff071f9c86dcffe195be65991089
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-07 10:18:42 -0400

    Starts session and root controllers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require supermodel
+//= require wisdom_crowd
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
+//= require_tree ../../../vendor/assets/javascripts
 //= require_tree .

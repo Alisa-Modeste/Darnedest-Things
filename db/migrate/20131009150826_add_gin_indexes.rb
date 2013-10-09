50d90f54e2af1a6fb6baf39f04121eae5f937f7d
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-09 11:39:11 -0400

    Add indexes to the rails migrate files; Some tester queries

---
+++ db/migrate/20131009150826_add_gin_indexes.rb
@@ -0,0 +1,14 @@
+class AddGinIndexes < ActiveRecord::Migration
+  def up
+    execute "CREATE INDEX index_questions_on_search_title ON questions USING gin(to_tsvector('english', title))"
+    execute "CREATE INDEX index_questions_on_search_body ON questions USING gin(to_tsvector('english', body))"
+    execute "CREATE INDEX index_questions_on_search_questions ON questions USING gin(to_tsvector('english', title || '' || coalesce(body,'')))"
+  end
+
+  def down
+    execute "DROP INDEX index_questions_on_search_title"
+    execute "DROP INDEX index_questions_on_search_body"
+    execute "DROP INDEX index_questions_on_search_questions"
+  end
+
+end
---
+++ tester_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE name;
+\c name
+\c tester01
+
+CREATE TABLE name (
+  col_name type
+)
+
+INSERT
+
+
+\d pg_index

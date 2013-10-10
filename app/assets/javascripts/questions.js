ba46577b69cf5c7e8f102020ea8295e958fd97f4
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-10 10:46:27 -0400

    Integrating tagit

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
 	var tags = $("#bootstrapped_tags").text().split(',');
+  console.log("tags are", tags)
+
+  $("#question_tags").tagit({
+    availableTags: tags
+  });
 
 });

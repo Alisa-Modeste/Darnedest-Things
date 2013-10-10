f25b4e2d7e9f2a2f4520bcc61a208efce841e32d
Alisa Modeste <user@alisamodeste.com>
2013-10-09 21:08:09 -0400

    Better grabbing of tag names2 - Files weren't deleted

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
 
-  // var tags = [];
-  // // $("#bootstrapped_tags")[0].html().split(/\D/).forEach(function(tag){
-  // $("#bootstrapped_tags").html().split(',').forEach(function(tag){
-          // tags.push(tag.split(/&quot;/)[1])
-    // });
-
-	var t = $("#bootstrapped_tags") //.html();
-	console.log(t)
-	console.log(t.attr('id'))
-	console.log(t.html())
-	console.log(t.val())
-	// var tags = JSON.parse(t);
-	// console.log(tags)
+	var tags = $("#bootstrapped_tags")
 });

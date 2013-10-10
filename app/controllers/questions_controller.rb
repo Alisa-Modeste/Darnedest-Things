54eae96a01d631c6c5fc1beb589c03338776dad9
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-10 11:34:54 -0400

    Can save tags to questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,13 +13,39 @@ class QuestionsController < ApplicationController
     @tags = Tag.all.map do |tag|
       tag.name
     end
-	
+
     render :new
   end
 
   def create
     question = Question.new(params[:question])
     question.user_id = self.current_user.id
+
+    tags = params[:question_tags]
+    tags = tags.split(",")
+    p "These are the tags", tags
+    #tag_ids = Tag.where(name: tags).select(:id).map{ |x| x.id }
+    #found_tags = Tag.where(name: tags).select([:id, :name]).map{ |x| {id: x.id, name: x.name} }
+    found_tag_ids = []
+    found_tag_names = []
+
+    Tag.where(name: tags).select([:id, :name]).each do |x|
+      found_tag_ids << x.id
+      found_tag_names << x.name
+    end
+
+    tag_ids = []
+    tags.each do |tag|
+      if found_tag_names.include? tag
+        position = found_tag_names.index(tag)
+        tag_ids << found_tag_ids[position]
+      else
+        new_tag = Tag.create({name: tag })
+        tag_ids << new_tag.id
+      end
+    end
+
+    question.tag_ids = tag_ids
 
     if question.save
       redirect_to question_url(question.id)

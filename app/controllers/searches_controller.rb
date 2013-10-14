b7b8adc55d3013e59923b3c61178d12e40cd853d
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-14 10:50:51 -0400

    More live links

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,8 +2,11 @@ class SearchesController < ApplicationController
   def create
     search = params[:search]
     tags = params[:tags] == "" ? nil : params[:tags]
-    @results = Question.search_database(tags, search)
+    #@results = Question.search_database(tags, search)
    # render json: result
-    render :index
+    #render :index
+
+    @questions = Question.page(params[:page]).search_database(tags, search)
+    render "questions/index"
   end
 end

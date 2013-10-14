e8ea536389d0a58a834d0cdbbfff5bbe84227579
Alisa Modeste <onewebsitecreator@hotmail.com>
2013-10-14 10:43:51 -0400

    Search form in partial;CSS;Added concur indexes 2

---
+++ db/migrate/20131014141710_concurrent_indexes.rb
@@ -0,0 +1,15 @@
+class ConcurrentIndexes < ActiveRecord::Migration
+  def up
+
+    execute "commit;"
+    execute "CREATE INDEX CONCURRENTLY index_questions_on_search_title ON questions USING gin(to_tsvector('english', title))"
+    execute "CREATE INDEX CONCURRENTLY index_questions_on_search_body ON questions USING gin(to_tsvector('english', body))"
+    execute "CREATE INDEX CONCURRENTLY index_questions_on_search_questions ON questions USING gin(to_tsvector('english', title || '' || coalesce(body,'')))"
+  end
+
+  def down
+    execute "DROP INDEX index_questions_on_search_title"
+    execute "DROP INDEX index_questions_on_search_body"
+    execute "DROP INDEX index_questions_on_search_questions"
+  end
+end

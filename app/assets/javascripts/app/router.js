0582e9735f5c7fa6b5ba2397e7bf645ffac1ea76
Alisa Modeste <user@alisamodeste.com>
2013-10-18 22:00:19 -0400

    Decreased size of nav;Fixed urls

--- app/assets/javascripts/app/router.js
+++ app/assets/javascripts/app/router.js
@@ -1,20 +1,20 @@
 App.Router.map(function(){
 	
 	this.resource("questions", {path: "/questions"}, function(){
-		this.route('new', {path: "/questions/new"});
-		this.route('question', {path: "/questions/:question_id"});
+		this.route('new', {path: "/new"});
+		this.route('question', {path: "/:question_id"});
 
 	});
 	
 	this.resource("users", {path: "/users"}, function(){
-		this.route('new', {path: "/users/new"});
-		this.route('user', {path: "/users/:user_id"});
+		this.route('new', {path: "/new"});
+		this.route('user', {path: "/:user_id"});
 
 	});
 	
 	this.resource("tags", {path: "/tags"}, function(){
-		this.route('new', {path: "/tags/new"});
-		this.route('tag', {path: "/tags/:tag_id"});
+		this.route('new', {path: "/new"});
+		this.route('tag', {path: "/:tag_id"});
 
 	});
 

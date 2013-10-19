77038c6aa6c943a87778f5e8ba9d5d7e50b91350
Alisa Modeste <user@alisamodeste.com>
2013-10-19 09:09:38 -0400

    Cleaning up;Moved questions controller.js

--- app/assets/javascripts/app/controllers/questions_controller.js
+++ app/assets/javascripts/app/controllers/questions_controller.js
@@ -1,10 +1,14 @@
-App.QuestionNewController = Ember.ArrayController.extend({
+App.QuestionsNewController = Ember.ObjectController.extend({
   save: function(){
+  console.log("Saving")
     var newQuestion = App.Question.create({
       title: this.get('title'),
       body: this.get('body'),
       user: this.get('user_id')
     })
+	console.log(newQuestion)
+	
+	var tags = this.get('tags')
 
     newQuestion.save();
   }

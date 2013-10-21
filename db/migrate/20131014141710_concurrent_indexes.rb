18d8685d8a565047e8c13ab4423f2609bd758f66
Alisa Modeste <user@alisamodeste.com>
2013-10-20 21:20:03 -0400

    Some from the commits Oct 16 - 18

--- db/migrate/20131014141710_concurrent_indexes.rb
+++ db/migrate/20131014141710_concurrent_indexes.rb
@@ -1,6 +1,10 @@
 class ConcurrentIndexes < ActiveRecord::Migration
   def up
 
+    execute "DROP INDEX index_questions_on_search_title"
+    execute "DROP INDEX index_questions_on_search_body"
+    execute "DROP INDEX index_questions_on_search_questions" 
+
     execute "commit;"
     execute "CREATE INDEX CONCURRENTLY index_questions_on_search_title ON questions USING gin(to_tsvector('english', title))"
     execute "CREATE INDEX CONCURRENTLY index_questions_on_search_body ON questions USING gin(to_tsvector('english', body))"
@@ -11,5 +15,10 @@ class ConcurrentIndexes < ActiveRecord::Migration
     execute "DROP INDEX index_questions_on_search_title"
     execute "DROP INDEX index_questions_on_search_body"
     execute "DROP INDEX index_questions_on_search_questions"
+
+    execute "CREATE INDEX index_questions_on_search_title ON questions USING gin(to_tsvector('english', title))"
+    execute "CREATE INDEX index_questions_on_search_body ON questions USING gin(to_tsvector('english', body))"
+    execute "CREATE INDEX index_questions_on_search_questions ON questions USING gin(to_tsvector('english', title || '' || coalesce(body,'')))" 
+
   end
 end
